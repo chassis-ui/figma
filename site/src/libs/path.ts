463b465e807e7777f20b22f6d164b3d992640652
Ozgur Gunes <o.gunes@gmail.com>
2026-04-15 11:09:16 +0300

    update website

--- site/src/libs/path.ts
+++ site/src/libs/path.ts
@@ -15,11 +15,11 @@ export function getDocsRelativePath(docsPath: string) {
 }
 
 export function getChassisAssetsFsPath() {
-  return path.join(process.cwd(), 'vendor/assets/dist/web/chassis-docs/')
+  return path.join(process.cwd(), 'vendor/assets/dist/web/docs', 'chassis')
 }
 
 export function getChassisTokensFsPath() {
-  return path.join(process.cwd(), 'node_modules/@chassis-ui/tokens/dist/web/chassis-docs')
+  return path.join(process.cwd(), 'node_modules/@chassis-ui/tokens/dist/web/docs', 'chassis')
 }
 
 export function getChassisCSSFsPath() {
